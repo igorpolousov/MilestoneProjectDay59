1776c61496fff23a07cd0db6dfc6884124130af9
Igor Polousov <igorpolousov@gmail.com>
2021-10-26 15:10:13 +0300

    Initial Commit

---
+++ MilestoneProjectDay59/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MilestoneProjectDay59
+//
+//  Created by Igor Polousov on 26.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
